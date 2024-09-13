54670a3a7757f12684efb128f150df9041e64b9a
Joseph Monaghan <Joseph_Monaghan@outlook.com>
2024-09-13 10:48:28 -0700

    First complete build of imzML_Scout

--- imzML_explorer.py
+++ imzML_explorer.py
@@ -5,7 +5,8 @@ from tkinter import ttk, filedialog
 from RangeSlider.RangeSlider import RangeSliderV
 import os
 from matplotlib.figure import Figure 
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk) 
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_tools import ToolBase, ToolToggleBase 
 import warnings
 import numpy as np
 
@@ -25,19 +26,23 @@ def get_aspect_ratio(img_file):
     metadata = img_file.metadata.pretty()
     x_pix_size = metadata["scan_settings"]["scanSettings1"]["pixel size (x)"]
     y_pix_size = metadata["scan_settings"]["scanSettings1"]["pixel size y"]
-    return y_pix_size/x_pix_size, x_pix_size, y_pix_size
+
+    max_x_dimension = metadata["scan_settings"]["scanSettings1"]["max dimension x"]
+
+    return y_pix_size/x_pix_size, x_pix_size, y_pix_size, max_x_dimension
 
 def plot_ion_image(*_):
-    global raw_ion_image, aspect_ratio,x_pix,y_pix
+    global raw_ion_image, aspect_ratio,x_pix,y_pix, coordinate_map, imzML_object, max_x_dimension
     target_mz = float(mz_entry.get())
     tolerance = float(tolerance_entry.get())
     filename = file_entry.get()
     window=target_mz*tolerance/1e6
 
     with warnings.catch_warnings(action="ignore"):
+        coordinate_map = []
         imzML_object = imzmlp.ImzMLParser(filename=filename,parse_lib='lxml')
     ion_image = imzmlp.getionimage(imzML_object,target_mz,window)
-    [aspect_ratio, x_pix, y_pix] = get_aspect_ratio(imzML_object)
+    [aspect_ratio, x_pix, y_pix, max_x_dimension] = get_aspect_ratio(imzML_object)
 
     #Normalize ion image (or don't) as specified in GUI
     norm_method = normalization_method.get()
@@ -65,9 +70,15 @@ def update_ion_image(*_):
 
     ion_image = raw_ion_image
 
-    ion_image_max = np.max(ion_image)
-    low_cutoff = ion_image_max * low_thres
-    up_cutoff = ion_image_max * up_thres
+    ##Absolute threshold method
+    # ion_image_max = np.max(ion_image)
+    # low_cutoff = ion_image_max * low_thres
+    # up_cutoff = ion_image_max * up_thres
+
+    ##Percentile method (preferred)
+    low_cutoff = np.percentile(ion_image,low_thres*100)
+    up_cutoff = np.percentile(ion_image,up_thres*100)
+
 
     ion_image = np.where(ion_image > up_cutoff,up_cutoff,ion_image)
     ion_image = np.where(ion_image < low_cutoff,low_cutoff,ion_image)
@@ -98,6 +109,8 @@ def update_ion_image(*_):
 
     export_button=tk.Button(text="Export Image",bg=TEAL,highlightbackground=TEAL,command=lambda:export_image(fig))
     export_button.grid(row=7,column=0,columnspan=3)
+
+    fig.canvas.callbacks.connect('button_press_event',report_coordinates)
     
 def export_image(fig):
     file = filedialog.asksaveasfilename(initialdir=os.getcwd())
@@ -112,7 +125,7 @@ def export_image(fig):
 
 def check_normalization():
     if normalization_method.get() == "custom":
-        normalize_custom_entry.grid(row=2,column=5)
+        normalize_custom_entry.grid(row=2,column=6)
         normalize_custom_entry.bind("<Return>",plot_ion_image)
         normalize_custom_entry.bind("<FocusOut>",plot_ion_image)
     else:
@@ -126,10 +139,57 @@ def check_normalization():
     except:
         pass
 
+def report_coordinates(event):
+    global coordinate_map, max_x_dimension, x_pix
+    if event.xdata != None:
+        scans_per_line = int(max_x_dimension / x_pix)
+        line_scan_num = int(round(event.ydata,0))+1
+        scan_along_line = int(round(event.xdata,0))+1
+        scan_idx = line_scan_num*scans_per_line + scan_along_line
+        plot_mass_spectrum(scan_idx)
+    
 
 
-def plot_mass_spectrum():
-    pass
+def plot_mass_spectrum(scan_idx):
+    global imzML_object, mz, intensities
+    [mz, intensities] = imzML_object.getspectrum(scan_idx)
+
+    fig_spectrum = Figure(figsize=(4,4),dpi=100,facecolor=TEAL,layout='tight')
+
+    plot2 = fig_spectrum.add_subplot()
+    plot2.plot(mz,intensities)
+    plot2.set_xlabel("m/z")
+    plot2.set_ylabel("Intensity")
+
+    try:
+        canvas_mass_spectrum.destroy()
+    except:
+        pass
+
+    canvas_mass_spectrum = FigureCanvasTkAgg(fig_spectrum)
+    canvas_mass_spectrum.draw()
+    toolbar = NavigationToolbar2Tk(canvas_mass_spectrum, pack_toolbar=False)
+    toolbar.update()
+    canvas_mass_spectrum.get_tk_widget().grid(row=5,column=6,columnspan=3)
+    toolbar.grid(row=6,column=6,columnspan=3)
+
+    fig_spectrum.canvas.callbacks.connect("button_press_event",change_target_mz)
+
+def change_target_mz(event):
+    new_mz = event.xdata
+    match_idx = (np.abs(mz - new_mz)).argmin()
+    new_mz = mz[match_idx]
+
+    if new_mz != None:
+        mz_entry.delete(0,tk.END)
+        mz_entry.insert(0,round(new_mz,4))
+        plot_ion_image()
+
+
+
+
+
+
 
 window = tk.Tk()
 window.title("IMZML Explorer")
@@ -188,7 +248,7 @@ tic_method.grid(row=3,column=4)
 ##Slider
 v_top = tk.DoubleVar(value=0.9)
 v_bottom = tk.DoubleVar(value=0.05)
-v_slider = RangeSliderV(window,[v_bottom,v_top],padY=12,bgColor=TEAL,valueSide="RIGHT",font_color='#ffffff',font_family="Helvetica",line_s_color=BEIGE,digit_precision='.2f')
+v_slider = RangeSliderV(window,[v_bottom,v_top],padY=12,bgColor=TEAL,valueSide="RIGHT",font_color='#ffffff',font_family="Helvetica",line_s_color=BEIGE,digit_precision='.2f',step_size=0.01)
 v_slider.grid(row=4,column=4,rowspan=4)
 v_top.trace_add('write',update_ion_image)
 v_bottom.trace_add('write',update_ion_image)
