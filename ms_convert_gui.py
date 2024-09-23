4e48c213e9c05775c9b501e7d482eaf3ca42c49b
Joseph Monaghan <Joseph_Monaghan@outlook.com>
2024-09-23 09:33:52 -0700

    Basic build of msConvert MAC wrapper

--- ms_convert_gui.py
+++ ms_convert_gui.py
@@ -3,6 +3,8 @@ from tkinter import ttk, filedialog
 import os
 import threading
 import sys
+import docker
+from gui_functions import get_file_type
 
 ##Colors and FONTS
 TEAL = "#2da7ad"
@@ -23,7 +25,7 @@ def main(tgt_dir=""):
             CD_entry.insert(0,directory)
 
             populate_list(directory)
-            FILE_TYPE = get_file_types(directory)
+            #FILE_TYPE = get_file_types(directory)
     
     def populate_list(dir:str):
         """takes an argument dir and populates the UI listbox based on its contents
@@ -37,21 +39,59 @@ def main(tgt_dir=""):
                 file_list.insert(ticker,file)
                 ticker+=1
     
-    def get_file_types(dir) -> str:
-        """dir: pathname for active directory
-        returns file_type as a str
-        [taken as first non-hidden (i.e. doesn't start with ".") file in the directory]"""
-        files = os.listdir(dir) 
-        for file in files:
-            split_file = file.split(".")
-            file_type = split_file[-1]
+    # def get_file_types(dir) -> str:
+    #     """dir: pathname for active directory
+    #     returns file_type as a str
+    #     [taken as first non-hidden (i.e. doesn't start with ".") file in the directory]"""
+    #     files = os.listdir(dir) 
+    #     for file in files:
+    #         split_file = file.split(".")
+    #         file_type = split_file[-1]
 
-        file_type_label = tk.Label(window_msconvert,text=f"File type: .{file_type}",bg=TEAL,font=FONT)
-        file_type_label.grid(row=1,column=3,columnspan=3)
-        return file_type
+    #     file_type_label = tk.Label(window_msconvert,text=f"File type: .{file_type}",bg=TEAL,font=FONT)
+    #     file_type_label.grid(row=1,column=3,columnspan=3)
+    #     return file_type
 
     def call_msconvert():
-        pass
+        sl = "/"
+        path = CD_entry.get()
+        if "win" in sys.platform and sys.platform != "darwin":
+            print("This GUI is for Mac/Linux, please use the MSConvert GUI from Proteowizard for PCs")
+        else:
+            DOCKER_IMAGE = "chambm/pwiz-skyline-i-agree-to-the-vendor-licenses"
+            client = docker.from_env()
+            client.images.pull(DOCKER_IMAGE)
+
+            working_directory = path
+            file_type = get_file_type(path)
+
+            vol = {working_directory: {'bind': fr"{sl}{DOCKER_IMAGE}{sl}data", 'mode': 'rw'}}
+
+            comm = fr"wine msconvert {sl}{DOCKER_IMAGE}{sl}data{sl}*.{file_type} --zlib=off --mzML --64 --outdir {sl}{DOCKER_IMAGE}{sl}data --filter '"'peakPicking true 1-'"' --simAsSpectra --srmAsSpectra"
+            print(comm)
+
+            comm = fr"wine msconvert {sl}{DOCKER_IMAGE}{sl}data{sl}*.{file_type} --mzML --64 --outdir {sl}{DOCKER_IMAGE}{sl}data --filter '"'peakPicking true 1-'"'"
+
+            if zlib.get():
+                comm = comm + " --zlib=off"
+
+            if SIM_as_spectra.get():
+                comm = comm + " --simAsSpectra"
+
+            if SRM_as_spectra.get():
+                comm = comm + " --srmAsSpectra"
+
+            env_vars = {"WINEDEBUG": "-all"}
+            print(comm)
+            client.containers.run(
+                image=DOCKER_IMAGE,
+                environment=env_vars,
+                volumes = vol,
+                command=comm,
+                working_dir=working_directory,
+                auto_remove=True,
+                detach=True
+                )
 
 
 
@@ -64,18 +104,30 @@ def main(tgt_dir=""):
 
     ##Choose Directory Button
     CD_button = tk.Button(window_msconvert,text="Select Folder",bg=TEAL,highlightbackground=TEAL,command=get_path)
-    CD_button.grid(row=1,column=0)
+    CD_button.grid(row=0,column=0)
 
     CD_entry = tk.Entry(window_msconvert,text="Enter Directory Here",highlightbackground=TEAL,background=BEIGE,fg="black",justify='center')
-    CD_entry.grid(row=1,column=1)
+    CD_entry.grid(row=0,column=1)
 
     ##Processing buttons
-    convert_mzML = tk.Button(window_msconvert,text="Full Conversion",bg=TEAL,highlightbackground=TEAL,command=call_msconvert)
-    convert_mzML.grid(row=2,column=4)
+    convert_mzML = tk.Button(window_msconvert,text="Convert to mzML",bg=TEAL,highlightbackground=TEAL,command=call_msconvert)
+    convert_mzML.grid(row=3,column=4,columnspan=3)
 
     #Listbox for files in target folder
     file_list = tk.Listbox(window_msconvert,bg=BEIGE,fg="black",height=10,highlightcolor=TEAL,width=35,justify='left')
-    file_list.grid(row=0,column=4,rowspan=2,columnspan=3)
+    file_list.grid(row=0,column=4,rowspan=3,columnspan=3)
+
+    zlib = tk.BooleanVar(window_msconvert)
+    zlib_check = tk.Checkbutton(window_msconvert,text="zlib compression?",bg=TEAL,font=FONT,var=zlib)
+    zlib_check.grid(row=1,column=0,columnspan=2)
+
+    SIM_as_spectra = tk.BooleanVar(window_msconvert)
+    SIM_check = tk.Checkbutton(window_msconvert,text="SIM as spectra?",bg=TEAL,font=FONT,var=SIM_as_spectra)
+    SIM_check.grid(row=2,column=0,columnspan=2)
+
+    SRM_as_spectra = tk.BooleanVar(window_msconvert)
+    SRM_check = tk.Checkbutton(window_msconvert,text="SRM as spectra?",bg=TEAL,font=FONT,var=SRM_as_spectra)
+    SRM_check.grid(row=3,column=0,columnspan=2)
 
     window_msconvert.mainloop()
 
