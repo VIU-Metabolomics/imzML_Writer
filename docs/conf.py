5299c99f1abb35a9d21bfbb197d01a65515a1c8d
Joseph Monaghan <Joseph_Monaghan@outlook.com>
2025-02-13 07:03:18 -0800

    Initial autodoc config

--- docs/conf.py
+++ docs/conf.py
@@ -13,14 +13,22 @@ release = '0.1'
 
 import os
 import sys
+import sphinx_rtd_theme
 
-sys.path.insert(0,os.path.abspath('../imzML_Writer'))
+sys.path.insert(0,os.path.abspath())
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.autosummary',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -30,5 +38,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
